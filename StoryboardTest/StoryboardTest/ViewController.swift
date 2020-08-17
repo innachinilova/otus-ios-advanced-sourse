551681d5541ca374dc82d3a8b7456583f877b058
Inna <inna.chinilova@mail.ru>
2020-08-17 13:22:53 +0300

    добавлен проект без storyboard

---
+++ StoryboardTest/StoryboardTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardTest
+//
+//  Created by Борис Полевой on 17.08.2020.
+//  Copyright © 2020 Inna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+}
